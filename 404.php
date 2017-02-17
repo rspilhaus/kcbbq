fe3b51118de361a953f1483c9a48919184037242
rspilhaus <rspilhaus@gmail.com>
2017-02-17 15:48:16 -0600

    404 + style changes
    Apparently diagonal headers is a *thing* now.

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<section class="page-title">
+	<?php get_template_part( 'partials/partial', 'nav' ); ?>
+	<div class="container">
+		<h1 class="page-title__header">Page Not Found</h1>
+	</div><!--end .container-->
+</section><!--end .page-title-->
+
+
+<section class="page-content">
+	<div class="container">
+		<h3>All outta BBQ</h3>
+		<p>Somehow you&rsquo;ve wound up in the wrong place. Don&rsquo;t worry, it happens to the best of us. Seeing as how there&rsquo;s around 5 pages on this site, it shouldn&rsquo;t be too hard for you to find what you are looking for. Just pick one of the pages below and be on your way!</p>
+		<ul>
+			<li><a href="<?php bloginfo('url'); ?>">Home</a> - The home page</li>
+			<li><a href="<?php bloginfo('url'); ?>/kc-bbq-style">Style</a> - What makes KC BBQ unique</li>
+			<li><a href="<?php bloginfo('url'); ?>/kc-bbq-history">History</a> - Where KC BBQ came from</li>
+			<li><a href="<?php bloginfo('url'); ?>/kc-bbq-restaurant-guide">Restaurant Guide</a> - A list of all the restaurants</li>
+			<li><a href="<?php bloginfo('url'); ?>/credits">Credits &amp; References</a> - Credits for the site</li>
+		</ul>	
+	</div><!--end .container-->
+</section><!--end .page-content-->
+	        
+<?php get_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@
 		<p class="hero__subtitle">
 			BBQ is the best food in the world and Kansas City does it better than anyone else. Learn what makes it unique, the history behind it, and why it&rsquo;s worth an entire trip just to&nbsp;taste.
 		</p>
-		<a href="<?php bloginfo('url'); ?>/style" class="hero__cta btn btn-white">Learn Why</a>
+		<a href="<?php bloginfo('url'); ?>/kc-bbq-style" class="hero__cta btn btn-white">Learn Why</a>
 	</div><!--end .container-->
 </section><!--end .hero-->
 <?php get_footer(); ?>
