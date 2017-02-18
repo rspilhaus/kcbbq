2d471895939c32c373d9186d363c818f7a7bc1fe
rspilhaus <rspilhaus@gmail.com>
2017-02-18 15:31:22 -0600

    Don't do header scroll on homepage

--- build/js/production.js
+++ build/js/production.js
@@ -40,14 +40,8 @@ $(document).ready(function() {
 //! Scrolling Styles for header
 // -----------------------------------------------------------------
 
-var header = $("header"),
-	headerOffset = $("header").offset().top;
+var	headerOffset = $("header").offset().top;
 
-$(window).resize(function () {
-   var header = $("header"),
-   	   headerOffset = $("header").offset().top;
-});
-	  
 $(window).load(function(){
 	var scroll = $(window).scrollTop();
 		if (scroll >= headerOffset) {
--- src/js/components/menu-scroll.js
+++ src/js/components/menu-scroll.js
@@ -2,14 +2,8 @@
 //! Scrolling Styles for header
 // -----------------------------------------------------------------
 
-var header = $("header"),
-	headerOffset = $("header").offset().top;
+var	headerOffset = $("header").offset().top;
 
-$(window).resize(function () {
-   var header = $("header"),
-   	   headerOffset = $("header").offset().top;
-});
-	  
 $(window).load(function(){
 	var scroll = $(window).scrollTop();
 		if (scroll >= headerOffset) {
